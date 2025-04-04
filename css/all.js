7c3edab43fd60c389feffe179abc6f99dbeb26b0
toributik <toributik33@gmail.com>
2025-04-04 16:23:52 +0530

    Add files via upload

---
+++ css/all.js
@@ -0,0 +1,10 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCDZoBecmF5q3xibgtJm5bwOLyI9x7nzCU",
+  authDomain: "toribill.firebaseapp.com",
+  databaseURL: "https://toribill-default-rtdb.firebaseio.com",
+  projectId: "toribill",
+  storageBucket: "toribill.firebasestorage.app",
+  messagingSenderId: "887198449730",
+  appId: "1:887198449730:web:998881b905423af6886ddb"
+};
